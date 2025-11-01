1be1c93e5e2d40475413bb95b1f65d80f73caaad
llanostecnology <llamostechnology@gamil.com>
2025-11-01 13:36:19 -0500

    Tipos de datos en JavaScript

---
+++ js/index.js
@@ -0,0 +1,47 @@
+/*En JavaScript declaramos una variable asi:*/ 
+
+var numero = 10;
+ 
+/*En JavaScript podemos declarar el proceso en dos líneas:*/ 
+
+var numero;
+
+numero = 10;
+ 
+/*En JavaScript tenemos tipos de variables:*/ 
+
+/*Tipo entero:*/ 
+
+var numero = 7;
+
+/*Tipo flotante:*/
+
+var temperatura = 32.5;
+
+/*Tipo cadena de texto:*/
+
+var cadena = "Juan Pablo Llanos"
+
+var cadena_nueva = "Hola #67/ Mundo"
+
+/*Tipo booleano:*/
+
+var variablefalso = false;
+
+var variableverdadero = true;
+ 
+
+/*Tipo arreglo:*/
+
+var numeros = [1,2,3,4,5,6,7,8,9,10];
+
+
+/*para seleccionar una posición del arreglo en Javascript, hacemos lo siguiente:*/
+
+document.writ(numero[6]);
+
+
+
+
+
+
